87d63a902e6a0f8f47f62548280dd44be325fa76
pixwok <dev@pixwok.fr>
2026-06-12 13:42:45 +0200

    Add PUT categories

--- app/api/Categories.py
+++ app/api/Categories.py
@@ -24,11 +24,31 @@ def get_category(categorie_id: int, db: Session = Depends(get_db)):
 # Création catégorie
 @router.post("/", response_model=APIResponse[CategoryResponse])
 def create_category(category: ModifyCategory, db: Session = Depends(get_db)):
+    category_row = db.query(Categories).filter(Categories.name == category.name).first()
+    if category_row:
+        raise HTTPException(status_code=404, detail="Category already exist")
     data = Categories(name=category.name)
     db.add(data)
     db.commit()
     db.refresh(data)
     return success_response(CategoryResponse.model_validate(data))
+
+# Mise à jour d'une catégorie
+@router.put("/{categorie_id}", response_model=APIResponse[CategoryResponse])
+def edit_goal(categorie_id: int, category_edit: ModifyCategory, db: Session = Depends(get_db)):
+    category_row = db.query(Categories).filter(Categories.id == categorie_id).first()
+    if not category_row:
+        raise HTTPException(status_code=404, detail="Category not found")
+    if db.query(Categories).filter(Categories.name == category_edit.name).first():
+        raise HTTPException(status_code=404, detail="Category name already exist")
+    editfield = category_edit.model_dump(exclude_unset=True)
+    
+    for field, value in editfield.items():
+        setattr(category_row, field, value)
+
+    db.commit()
+    db.refresh(category_row)    
+    return success_response(CategoryResponse.model_validate(category_row))
 
 # Suppression catégorie
 @router.delete("/{categorie_id}", response_model=APIResponse[CategoryResponse])
